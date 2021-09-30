ec227e0d7c9ef519bd8d398c3a9e7c294baa567f
Isaac <isaac.ewing3@gmail.com>
2021-09-30 13:56:09 -0600

    almost done, undo is being weird

---
+++ src/components/app/App.jsx
@@ -0,0 +1,30 @@
+import React, { useReducer } from 'react';
+
+const history = {
+  before: [],
+  current: '#FF0000',
+  after: []
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'undo': return { after: [state.current, ...state.after], current: state.before[state.before.length - 1], before: state.before.slice(0, -1) };
+    case 'redo': return { before: [...state.before, state.current], current: state.after[0], after: state.after.slice(1) };
+    case 'record': return { before: [...state.before, state.current], current: action.payload };
+  }
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, history);
+
+  return (
+    <>
+      <button aria-label="undo-button" onClick={() => dispatch({ type: 'undo' })}>undo</button>
+      <button aria-label="redo-button" onClick={() => dispatch({ type: 'redo' })}>redo</button>
+      <input aria-label="color-picker" type="color" value={state.current} onChange={({ target }) => dispatch({ type: 'record', payload: target.value })} />
+      <div aria-label="display" style={{ backgroundColor: state.current, width: '10rem', height: '10rem' }}></div>
+    </>
+  );
+}
+
+export default App;
