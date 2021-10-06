91b58cceef40c2d33a4ff3d2f971020855d5271d
Isaac <isaac.ewing3@gmail.com>
2021-10-06 10:24:27 -0600

    passing test

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -10,7 +10,7 @@ const reducer = (state, action) => {
   switch (action.type) {
     case 'undo': return { after: [state.current, ...state.after], current: state.before[state.before.length - 1], before: state.before.slice(0, -1) };
     case 'redo': return { before: [...state.before, state.current], current: state.after[0], after: state.after.slice(1) };
-    case 'record': return { before: [...state.before, state.current], current: action.payload };
+    case 'record': return { ...state, before: [...state.before, state.current], current: action.payload };
   }
 };
 
@@ -21,8 +21,10 @@ function App() {
     <>
       <button aria-label="undo-button" onClick={() => dispatch({ type: 'undo' })}>undo</button>
       <button aria-label="redo-button" onClick={() => dispatch({ type: 'redo' })}>redo</button>
-      <input aria-label="color-picker" type="color" value={state.current} onChange={({ target }) => dispatch({ type: 'record', payload: target.value })} />
-      <div aria-label="display" style={{ backgroundColor: state.current, width: '10rem', height: '10rem' }}></div>
+      <label>Color Picker
+        <input aria-label="color-picker" type="color" value={state.current} onChange={({ target }) => dispatch({ type: 'record', payload: target.value })} />
+        </label>
+      <div aria-label="display" data-testid="test" style={{ backgroundColor: state.current, width: '10rem', height: '10rem' }}></div>
     </>
   );
 }
